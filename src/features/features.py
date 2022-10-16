8bd59eade54dae8d4381d06a152370231ebd3f4d
Mikhail Bogomazov <ihackit595@gmail.com>
2022-10-16 21:42:08 +0300

    add feature generating step

--- src/features/features.py
+++ src/features/features.py
@@ -1,6 +1,37 @@
 import pandas as pd
+import datetime as dt
+from src.config import *
 
 
-def add_early_wakeup(df: pd.DataFrame) -> pd.DataFrame:
-    pass
+def add_sleeping_mode_features(df):
+    for ind, row in df.iterrows():
+        fall_asleep_time = dt.datetime.strptime(
+            row[FALL_ASLEEP_COL], '%H:%M:%S')
+        wake_up_time = dt.datetime.strptime(
+            row[WAKE_UP_TIME_COL], '%H:%M:%S')
+        if fall_asleep_time.hour > 12:
+            wake_up_time += dt.timedelta(days=1)
+
+        df.at[ind, SLEEP_DURATION_COL] = round(
+            (wake_up_time - fall_asleep_time).total_seconds() / 3600, 3)
+
+        if fall_asleep_time.hour > 12 or wake_up_time.hour < 8:
+            df.at[ind, SLEEP_BEHAVIORS_COL] = '0'
+        elif fall_asleep_time.hour < 12 and wake_up_time.hour > 8:
+            df.at[ind, SLEEP_BEHAVIORS_COL] = '1'
+        else:
+            df.at[ind, SLEEP_BEHAVIORS_COL] = '2'
+
+    return df
+
+
+def remove_unnecessary_columns(df: pd.DataFrame) -> pd.DataFrame:
+    df.drop([WAKE_UP_TIME_COL, FALL_ASLEEP_COL], axis=1, inplace=True)
+    return df
+
+
+def gen_features(df: pd.DataFrame) -> pd.DataFrame:
+    df = add_sleeping_mode_features(df)
+    df = remove_unnecessary_columns(df)
+
     return df
